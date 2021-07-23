fd3234d78dcfd671d840f1684671d5c56eede268
Sravya <65549677+Sravya2007@users.noreply.github.com>
2021-07-23 21:17:28 +0530

    Flask Mockup 2

---
+++ storage.py
@@ -0,0 +1,13 @@
+import csv
+
+all_articles = []
+
+with open('articles.csv', encoding = "utf-8") as f:
+    csv_reader = csv.reader(f)
+    article_data = list(csv_reader)
+    column_names = article_data[0]
+    for values in article_data[1:]:
+        all_articles.append(dict(zip(column_names, values)))
+
+liked_articles = []
+not_liked_articles = []
